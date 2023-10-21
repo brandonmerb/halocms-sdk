602a356ccfb5db30ada0e62460adc4bf2883ba53
Brandon Erb <brandon.m.erb@gmail.com>
2023-10-20 20:54:52 -0400

    Package fixes + DI testing

--- src/modules/layout/layouts/base.layout.vue
+++ src/modules/layout/layouts/base.layout.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
+  <div class="card">
     This indicates that we're running!!!
   </div>
+  <div class="card">
+    Second value: 
+  </div>
 </template>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { DependencyInjectionMiddleware, Logger, LoggerInterface } from '@atomicdesign/atomic-singularity';
+
+onMounted(() => {
+  const l = DependencyInjectionMiddleware.instance.inject<LoggerInterface>(Logger);
+  l.info("This works!!");
+})
+
+</script>
+<style>
+.card {
+  background-color: #FFF !important;
+}
+</style>
