11a043f72429ff73ba500645e2eab86489472e56
Brandon Erb <brandon.m.erb@gmail.com>
2023-11-09 11:06:51 -0500

    configuration testing ideas

--- src/modules/vuetify/vuetify.nebula.ts
+++ src/modules/vuetify/vuetify.nebula.ts
@@ -4,10 +4,49 @@ import { createNebula } from "@atomicdesign/atomic-singularity";
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
-export const useVuetify = createNebula<HaloCMSNebula>({
+interface Test1 {
+  evenCoolerKey: string;
+}
+
+interface Test2 {
+  other: number;
+  mainKey: {
+    subKey: string;
+  }
+}
+
+export const useVuetify = createNebula<HaloCMSNebula, Test2 & Test1>({
   name: "Halo CMS SDK: Vuetify",
 
   vuePlugins: [
-    createVuetify({})
+    createVuetify({
+      theme: {
+        themes: {
+          light: {
+            variables: {
+              "theme-background": "#F4F4F4"
+            }
+          }
+        }
+      }
+    })
   ]
 }).build();
+  // .addConfigurable("")
+  // .addConfiguration("activeTheme", "light", "light")
+  // .addConfiguration("")
+  // .build();
+
+
+  // export const useMadeup = createNebula({
+  //   name: "asd",
+
+  //   // configuration: {
+  //   //   testValue: "asdasd"
+  //   // }
+  //   configure: [
+  //     useVuetify
+  //       .addConfigurableOption("evenCoolerKey", "test")
+  //       .addConfigurableOption("other", 3)
+  //     ]
+  // });
